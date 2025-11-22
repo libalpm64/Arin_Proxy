97a2bb578650eabed383564c5a347b976801d998
libalpm64 <159192189+libalpm64@users.noreply.github.com>
2025-11-22 10:37:04 -0500

    Arin Proxy V3.0

---
+++ may_http/src/http_server.rs
@@ -0,0 +1,236 @@
+//! http server implementation on top of `MAY`
+
+use std::io::{self, Read, Write};
+use std::mem::MaybeUninit;
+use std::net::ToSocketAddrs;
+
+use crate::request::{self, Request};
+use crate::response::{self, Response};
+
+#[cfg(unix)]
+use bytes::Buf;
+use bytes::{BufMut, BytesMut};
+#[cfg(unix)]
+use may::io::WaitIo;
+use may::net::{TcpListener, TcpStream};
+use may::{coroutine, go};
+
+macro_rules! t_c {
+    ($e: expr) => {
+        match $e {
+            Ok(val) => val,
+            Err(err) => {
+                error!("call = {:?}\nerr = {:?}", stringify!($e), err);
+                continue;
+            }
+        }
+    };
+}
+
+/// the http service trait
+/// user code should supply a type that impl the `call` method for the http server
+///
+pub trait HttpService {
+    fn call(&mut self, req: Request, rsp: &mut Response) -> io::Result<()>;
+}
+
+pub trait HttpServiceFactory: Send + Sized + 'static {
+    type Service: HttpService + Send;
+    // create a new http service for each connection
+    fn new_service(&self, id: usize) -> Self::Service;
+
+    /// Spawns the http service, binding to the given address
+    /// return a coroutine that you can cancel it when need to stop the service
+    fn start<L: ToSocketAddrs>(self, addr: L) -> io::Result<coroutine::JoinHandle<()>> {
+        let listener = TcpListener::bind(addr)?;
+        go!(
+            coroutine::Builder::new().name("TcpServerFac".to_owned()),
+            move || {
+                #[cfg(unix)]
+                use std::os::fd::AsRawFd;
+                #[cfg(windows)]
+                use std::os::windows::io::AsRawSocket;
+                for stream in listener.incoming() {
+                    let mut stream = t_c!(stream);
+                    #[cfg(unix)]
+                    let id = stream.as_raw_fd() as usize;
+                    #[cfg(windows)]
+                    let id = stream.as_raw_socket() as usize;
+                    // t_c!(stream.set_nodelay(true));
+                    let service = self.new_service(id);
+                    let builder = may::coroutine::Builder::new().id(id);
+                    go!(
+                        builder,
+                        move || if let Err(e) = each_connection_loop(&mut stream, service) {
+                            error!("service err = {e:?}");
+                            stream.shutdown(std::net::Shutdown::Both).ok();
+                        }
+                    )
+                    .unwrap();
+                }
+            }
+        )
+    }
+}
+
+#[inline]
+#[cold]
+pub(crate) fn err<T>(e: io::Error) -> io::Result<T> {
+    Err(e)
+}
+
+#[cfg(unix)]
+#[inline]
+fn nonblock_read(stream: &mut impl Read, req_buf: &mut BytesMut) -> io::Result<bool> {
+    reserve_buf(req_buf);
+    let read_buf: &mut [u8] = unsafe { std::mem::transmute(req_buf.chunk_mut()) };
+    let len = read_buf.len();
+
+    let mut read_cnt = 0;
+    while read_cnt < len {
+        match stream.read(unsafe { read_buf.get_unchecked_mut(read_cnt..) }) {
+            Ok(0) => return err(io::Error::new(io::ErrorKind::BrokenPipe, "read closed")),
+            Ok(n) => read_cnt += n,
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+            Err(e) => return err(e),
+        }
+    }
+
+    unsafe { req_buf.advance_mut(read_cnt) };
+    Ok(read_cnt < len)
+}
+
+#[cfg(unix)]
+#[inline]
+fn nonblock_write(stream: &mut impl Write, rsp_buf: &mut BytesMut) -> io::Result<usize> {
+    let write_buf = rsp_buf.chunk();
+    let len = write_buf.len();
+    let mut write_cnt = 0;
+    while write_cnt < len {
+        match stream.write(unsafe { write_buf.get_unchecked(write_cnt..) }) {
+            Ok(0) => return err(io::Error::new(io::ErrorKind::BrokenPipe, "write closed")),
+            Ok(n) => write_cnt += n,
+            Err(e) if e.kind() == io::ErrorKind::WouldBlock => break,
+            Err(e) => return err(e),
+        }
+    }
+    rsp_buf.advance(write_cnt);
+    Ok(write_cnt)
+}
+
+const BUF_LEN: usize = 4096 * 8;
+#[inline]
+pub(crate) fn reserve_buf(buf: &mut BytesMut) {
+    let rem = buf.capacity() - buf.len();
+    if rem < 1024 {
+        buf.reserve(BUF_LEN - rem);
+    }
+}
+
+/// this is the generic type http server
+/// with a type parameter that impl `HttpService` trait
+///
+pub struct HttpServer<T>(pub T);
+
+#[cfg(unix)]
+fn each_connection_loop<T: HttpService>(stream: &mut TcpStream, mut service: T) -> io::Result<()> {
+    let mut req_buf = BytesMut::with_capacity(BUF_LEN);
+    let mut rsp_buf = BytesMut::with_capacity(BUF_LEN);
+    let mut body_buf = BytesMut::with_capacity(4096);
+
+    loop {
+        let read_blocked = nonblock_read(stream.inner_mut(), &mut req_buf)?;
+
+        // prepare the requests, we should make sure the request is fully read
+        loop {
+            let mut headers = [MaybeUninit::uninit(); request::MAX_HEADERS];
+            let req = match request::decode(&mut headers, &mut req_buf, stream)? {
+                Some(req) => req,
+                None => break,
+            };
+            reserve_buf(&mut rsp_buf);
+            let mut rsp = Response::new(&mut body_buf);
+            match service.call(req, &mut rsp) {
+                Ok(()) => response::encode(rsp, &mut rsp_buf),
+                Err(e) => {
+                    eprintln!("service err = {e:?}");
+                    response::encode_error(e, &mut rsp_buf);
+                }
+            }
+            // here need to use no_delay tcp option
+            // nonblock_write(stream.inner_mut(), &mut rsp_buf)?;
+        }
+
+        // write out the responses
+        nonblock_write(stream.inner_mut(), &mut rsp_buf)?;
+
+        if read_blocked {
+            stream.wait_io();
+        }
+    }
+}
+
+#[cfg(not(unix))]
+fn each_connection_loop<T: HttpService>(stream: &mut TcpStream, mut service: T) -> io::Result<()> {
+    let mut req_buf = BytesMut::with_capacity(BUF_LEN);
+    let mut rsp_buf = BytesMut::with_capacity(BUF_LEN);
+    let mut body_buf = BytesMut::with_capacity(BUF_LEN);
+    loop {
+        // read the socket for requests
+        reserve_buf(&mut req_buf);
+        let read_buf: &mut [u8] = unsafe { std::mem::transmute(&mut *req_buf.chunk_mut()) };
+        let read_cnt = stream.read(read_buf)?;
+        if read_cnt == 0 {
+            //connection was closed
+            return err(io::Error::new(io::ErrorKind::BrokenPipe, "closed"));
+        }
+        unsafe { req_buf.advance_mut(read_cnt) };
+
+        // prepare the requests
+        if read_cnt > 0 {
+            loop {
+                let mut headers = [MaybeUninit::uninit(); request::MAX_HEADERS];
+                let req = match request::decode(&mut headers, &mut req_buf, stream)? {
+                    Some(req) => req,
+                    None => break,
+                };
+                let mut rsp = Response::new(&mut body_buf);
+                match service.call(req, &mut rsp) {
+                    Ok(()) => response::encode(rsp, &mut rsp_buf),
+                    Err(e) => {
+                        eprintln!("service err = {:?}", e);
+                        response::encode_error(e, &mut rsp_buf);
+                    }
+                }
+            }
+        }
+
+        // send the result back to client
+        stream.write_all(&rsp_buf)?;
+    }
+}
+
+impl<T: HttpService + Clone + Send + Sync + 'static> HttpServer<T> {
+    /// Spawns the http service, binding to the given address
+    /// return a coroutine that you can cancel it when need to stop the service
+    pub fn start<L: ToSocketAddrs>(self, addr: L) -> io::Result<coroutine::JoinHandle<()>> {
+        let listener = TcpListener::bind(addr)?;
+        let service = self.0;
+        go!(
+            coroutine::Builder::new().name("TcpServer".to_owned()),
+            move || {
+                for stream in listener.incoming() {
+                    let mut stream = t_c!(stream);
+                    // t_c!(stream.set_nodelay(true));
+                    let service = service.clone();
+                    go!(
+                        move || if let Err(e) = each_connection_loop(&mut stream, service) {
+                            error!("service err = {e:?}");
+                            stream.shutdown(std::net::Shutdown::Both).ok();
+                        }
+                    );
+                }
+            }
+        )
+    }
+}
---
+++ may_http/src/request.rs
@@ -0,0 +1,183 @@
+use std::fmt;
+use std::io::{self, BufRead, Read};
+use std::mem::MaybeUninit;
+
+pub(crate) const MAX_HEADERS: usize = 64;
+
+use bytes::{Buf, BufMut, BytesMut};
+use may::net::TcpStream;
+
+use crate::http_server::err;
+
+pub struct BodyReader<'buf, 'stream> {
+    // remaining bytes for body
+    req_buf: &'buf mut BytesMut,
+    // the max body length limit
+    body_limit: usize,
+    // total read count
+    total_read: usize,
+    // used to read extra body bytes
+    stream: &'stream mut TcpStream,
+}
+
+impl BodyReader<'_, '_> {
+    fn read_more_data(&mut self) -> io::Result<usize> {
+        crate::http_server::reserve_buf(self.req_buf);
+        let read_buf: &mut [u8] = unsafe { std::mem::transmute(self.req_buf.chunk_mut()) };
+        let n = self.stream.read(read_buf)?;
+        unsafe { self.req_buf.advance_mut(n) };
+        Ok(n)
+    }
+}
+
+impl Read for BodyReader<'_, '_> {
+    // the user should control the body reading, don't exceeds the body!
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        if self.total_read >= self.body_limit {
+            return Ok(0);
+        }
+
+        loop {
+            if !self.req_buf.is_empty() {
+                let min_len = buf.len().min(self.body_limit - self.total_read);
+                let n = self.req_buf.reader().read(&mut buf[..min_len])?;
+                self.total_read += n;
+                return Ok(n);
+            }
+
+            if self.read_more_data()? == 0 {
+                return Ok(0);
+            }
+        }
+    }
+}
+
+impl BufRead for BodyReader<'_, '_> {
+    fn fill_buf(&mut self) -> io::Result<&[u8]> {
+        let remain = self.body_limit - self.total_read;
+        if remain == 0 {
+            return Ok(&[]);
+        }
+        if self.req_buf.is_empty() {
+            self.read_more_data()?;
+        }
+        let n = self.req_buf.len().min(remain);
+        Ok(&self.req_buf.chunk()[0..n])
+    }
+
+    fn consume(&mut self, amt: usize) {
+        assert!(amt <= self.body_limit - self.total_read);
+        assert!(amt <= self.req_buf.len());
+        self.total_read += amt;
+        self.req_buf.advance(amt)
+    }
+}
+
+impl Drop for BodyReader<'_, '_> {
+    fn drop(&mut self) {
+        // consume all the remaining bytes
+        while let Ok(n) = self.fill_buf().map(|b| b.len()) {
+            if n == 0 {
+                break;
+            }
+            // println!("drop: {:?}", n);
+            self.consume(n);
+        }
+    }
+}
+
+// we should hold the mut ref of req_buf
+// before into body, this req_buf is only for holding headers
+// after into body, this req_buf is mutable to read extra body bytes
+// and the headers buf can be reused
+pub struct Request<'buf, 'header, 'stream> {
+    req: httparse::Request<'header, 'buf>,
+    req_buf: &'buf mut BytesMut,
+    stream: &'stream mut TcpStream,
+}
+
+impl<'buf, 'stream> Request<'buf, '_, 'stream> {
+    pub fn method(&self) -> &str {
+        self.req.method.unwrap()
+    }
+
+    pub fn path(&self) -> &str {
+        self.req.path.unwrap()
+    }
+
+    pub fn version(&self) -> u8 {
+        self.req.version.unwrap()
+    }
+
+    pub fn headers(&self) -> &[httparse::Header<'_>] {
+        self.req.headers
+    }
+
+    pub fn body(self) -> BodyReader<'buf, 'stream> {
+        BodyReader {
+            body_limit: self.content_length(),
+            total_read: 0,
+            stream: self.stream,
+            req_buf: self.req_buf,
+        }
+    }
+
+    pub fn body_ref<'a>(&'a mut self) -> BodyReader<'a, 'a> {
+        BodyReader {
+            body_limit: self.content_length(),
+            total_read: 0,
+            stream: self.stream,
+            req_buf: self.req_buf,
+        }
+    }
+
+
+    fn content_length(&self) -> usize {
+        let mut len = 0;
+        for header in self.req.headers.iter() {
+            if header.name.eq_ignore_ascii_case("content-length") {
+                len = std::str::from_utf8(header.value).unwrap().parse().unwrap();
+                break;
+            }
+        }
+        len
+    }
+}
+
+impl fmt::Debug for Request<'_, '_, '_> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<HTTP Request {} {}>", self.method(), self.path())
+    }
+}
+
+pub fn decode<'header, 'buf, 'stream>(
+    headers: &'header mut [MaybeUninit<httparse::Header<'buf>>; MAX_HEADERS],
+    req_buf: &'buf mut BytesMut,
+    stream: &'stream mut TcpStream,
+) -> io::Result<Option<Request<'buf, 'header, 'stream>>> {
+    let mut req = httparse::Request::new(&mut []);
+    // safety: don't hold the reference of req_buf
+    // so we can transfer the mutable reference to Request
+    let buf: &[u8] = unsafe { std::mem::transmute(req_buf.chunk()) };
+    let status = match req.parse_with_uninit_headers(buf, headers) {
+        Ok(s) => s,
+        Err(e) => {
+            let msg = format!("failed to parse http request: {e:?}");
+            eprintln!("{msg}");
+            return err(io::Error::other(msg));
+        }
+    };
+
+    let len = match status {
+        httparse::Status::Complete(amt) => amt,
+        httparse::Status::Partial => return Ok(None),
+    };
+    req_buf.advance(len);
+
+    // println!("req: {:?}", std::str::from_utf8(req_buf).unwrap());
+    Ok(Some(Request {
+        req,
+        req_buf,
+        stream,
+    }))
+}
